31709821796dee901827c9a520b814f5118ec6b6
ljubicamiljkovic56 <ljubica.m19@gmail.com>
2023-02-21 22:18:59 +0100

    Restaurant and Person classes finished

--- DeliveryApp2023/src/peoplePackage/Person.java
+++ DeliveryApp2023/src/peoplePackage/Person.java
@@ -1,5 +1,87 @@
 package peoplePackage;
 
-public class Person {
+import enumerations.Gender;
 
+public class Person {
+	
+	protected String name;
+	protected String surname;
+	protected Gender gender;
+	protected String username;
+	protected String password;
+	
+	public Person() {
+		this.name = "";
+		this.surname = "";
+		this.gender = Gender.female;
+		this.username = "";
+		this.password = "";
+	}
+
+	public Person(String name, String surname, Gender gender, String username, String password) {
+		super();
+		this.name = name;
+		this.surname = surname;
+		this.gender = gender;
+		this.username = username;
+		this.password = password;
+	}
+	
+	public Person(Person original) {
+		this.name = original.name;
+		this.surname = original.surname;
+		this.gender = original.gender;
+		this.username = original.username;
+		this.password = original.password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public Gender getGender() {
+		return gender;
+	}
+
+	public void setGender(Gender gender) {
+		this.gender = gender;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "Person "
+				+ "name=" + name + 
+				", surname=" + surname + 
+				", gender=" + gender + 
+				", username=" + username
+				+ ", password=" + password + " ";
+	}
 }
--- DeliveryApp2023/src/restaurantPackage/Restaurant.java
+++ DeliveryApp2023/src/restaurantPackage/Restaurant.java
@@ -1,5 +1,64 @@
 package restaurantPackage;
 
-public class Restaurant {
+import enumerations.RestaurantEnum;
 
+public class Restaurant {
+	
+	protected String name;
+	protected String address;
+	protected RestaurantEnum category;
+	
+	public Restaurant() {
+		this.name = "";
+		this.address = "";
+		this.category = RestaurantEnum.Pizzeria;
+	}
+	
+	public Restaurant(String name, String address, RestaurantEnum category) {
+		super();
+		this.name = name;
+		this.address = address;
+		this.category = category;
+	}
+
+	public Restaurant(Restaurant original) {
+		this.name = original.name;
+		this.address = original.address;
+		this.category = original.category;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public RestaurantEnum getCategory() {
+		return category;
+	}
+	
+	public void setCategory(RestaurantEnum category) {
+		this.category = category;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Restaurant "
+				+ "name=" + name + 
+				", address=" + address + 
+				", category=" + category + " ";
+	}
+	
+	
 }
